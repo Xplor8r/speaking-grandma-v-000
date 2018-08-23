9288f26185129705a9912e2af19717dcee9853cc
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-23 03:46:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 def speak_to_grandma(string)
   if string.upcase
-    puts "NO, NOT SINCE 1938!"
+    puts 'NO, NOT SINCE 1938!'
   elsif string == 'I LOVE YOU GRANDMA!'
-    
-
+    puts 'I LOVE YOU TOO PUMPKIN!'
+  else
+    puts 'HUH?! SPEAK UP, SONNY!'
+  end
 end
 
 # Whatever you say to grandma, she should respond with
