01edd6b62bfa674b9d123a7a07ea1c31f3e76a1a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-23 03:52:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(string)
-  if string == string.upcase()
-    puts "NO, NOT SINCE 1938!"
+  if string != string.upcase()
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts 
   end
 end
 
